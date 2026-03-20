bb2c4a5b30a7e18d61839fb187de90ae0d6dd7c1
swapno963 <swapno963@gmail.com>
2026-03-20 19:53:22 +0600

    Bug fixed

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "vpc" {
 
 # Security Group for the Public Instance
 resource "aws_security_group" "public_sg" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = module.vpc.vpc_id
 
   ingress {
     from_port   = 22
@@ -57,7 +57,7 @@ resource "aws_security_group" "public_sg" {
 
 # Security Group for the Private Instance
 resource "aws_security_group" "private_sg" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = module.vpc.vpc_id
 
   ingress {
     from_port   = 22
@@ -78,7 +78,109 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
+##########################
+# 1. Security Group for ALB
+##########################
+resource "aws_security_group" "alb_sg" {
+  vpc_id = module.vpc.vpc_id  # Use your VPC ID from module
 
+  description = "Allow HTTP from anywhere"
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+#######################################
+# 2. Security Group Update for Private Instance
+# Allow HTTP traffic from ALB
+#######################################
+resource "aws_security_group_rule" "allow_alb_to_private" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.private_sg.id
+  source_security_group_id = aws_security_group.alb_sg.id
+  description              = "Allow HTTP from ALB"
+}
+
+##########################
+# 3. Create Application Load Balancer
+##########################
+resource "aws_lb" "frontend_alb" {
+  name               = "frontend-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnets  # Public subnet
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "frontend-alb"
+  }
+}
+
+##########################
+# 4. Target Group for Private Frontend Instance
+##########################
+resource "aws_lb_target_group" "frontend_tg" {
+  name     = "frontend-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+
+  tags = {
+    Name = "frontend-tg"
+  }
+}
+
+###################################
+# 5. Attach Private Instance to Target Group
+###################################
+resource "aws_lb_target_group_attachment" "frontend_attachment" {
+  target_group_arn = aws_lb_target_group.frontend_tg.arn
+  target_id        = aws_instance.private.id  # Replace with your frontend instance
+  port             = 80
+}
+
+##########################
+# 6. ALB Listener
+##########################
+resource "aws_lb_listener" "frontend_listener" {
+  load_balancer_arn = aws_lb.frontend_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+  }
+}
 # Key Pair
 resource "aws_key_pair" "main" {
   key_name   = "main-key"
@@ -91,7 +193,7 @@ resource "aws_key_pair" "main" {
 resource "aws_instance" "public" {
   ami           = "ami-060e277c0d4cce553"  # Ubuntu AMI
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = module.vpc.public.id
   key_name      = aws_key_pair.main.key_name
 
   tags = {
@@ -105,7 +207,7 @@ resource "aws_instance" "public" {
 resource "aws_instance" "private" {
   ami           = "ami-060e277c0d4cce553"  # Ubuntu AMI
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.private.id
+  subnet_id     = module.vpc.private.id
   key_name      = aws_key_pair.main.key_name
   user_data = <<-EOF
               #!/bin/bash
