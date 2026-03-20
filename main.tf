42f65cfab5cdfe1f80d5af30b25288136d808192
swapno963 <swapno963@gmail.com>
2026-03-20 17:46:27 +0600

    Created vpc, public-private subnet, security group and 2 instance is created and one ec2 will be responsible for jump host(basion) and another one is for runnign application

---
+++ main.tf
@@ -0,0 +1,116 @@
+# main.tf
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.0"  # Specify the version of the module
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.2.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # Explicitly enable auto-assign public IPv4 address on public subnets
+  map_public_ip_on_launch = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+
+
+# Security Group for the Public Instance
+resource "aws_security_group" "public_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public-sg"
+  }
+}
+
+# Security Group for the Private Instance
+resource "aws_security_group" "private_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]  # Only allow SSH from the public subnet
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "private-sg"
+  }
+}
+
+
+# Key Pair
+resource "aws_key_pair" "main" {
+  key_name   = "main-key"
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with your own public key
+}
+
+
+
+# Public EC2 Instance
+resource "aws_instance" "public" {
+  ami           = "ami-060e277c0d4cce553"  # Ubuntu AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  key_name      = aws_key_pair.main.key_name
+
+  tags = {
+    Name = "public-instance"
+  }
+
+  security_groups = [aws_security_group.public_sg.name]
+}
+
+# Private EC2 Instance
+resource "aws_instance" "private" {
+  ami           = "ami-060e277c0d4cce553"  # Ubuntu AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private.id
+  key_name      = aws_key_pair.main.key_name
+
+  tags = {
+    Name = "private-instance"
+  }
+
+  security_groups = [aws_security_group.private_sg.name]
+}
